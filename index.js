99b2ade158a006d3826d3bb34f24d9b261071409
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-24 16:31:05 -0600

    reworked code

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // linking inquirer package
 const { prompt } = require("inquirer");
 // linking Database
-const db = require("./db/database");
+const connection = require("./db/database");
 // linking counsole tale. package
 require("console.table");
 // linking mysql package
-
-
 function init() {
   prompt([
     {
@@ -32,26 +30,79 @@ function init() {
         }
       ]
     }
-  ]).then(res =>{
+  ]).then(res => {
     let choice = res.choice;
-  
-  switch (choice) {
-    case "VIEW_EMPLOYEES":
-      viewEmployees();
-      break;
-    case "ADD_EMPLOYEE":
-      addEmployee();
-      break;
-    case "REMOVE_EMPLOYEE":
-      removeEmployee()
-    break;
-    default:
-      quit();
-
+    switch (choice) {
+      case "VIEW_EMPLOYEES":
+        viewEmployees();
+        break;
+      case "ADD_EMPLOYEE":
+        addEmployee();
+        break;
+      case "REMOVE_EMPLOYEE":
+        removeEmployee()
+        break;
+      case "QUIT":
+        connection.end();
+      default:
+        break;
+    }
+  });
+}
+  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  function viewEmployees() {
+    connection.query("SELECT * FROM employee", (err, result) => {
+      console.table(result);
+      init();
+    });
   }
-}
-  ).catch(console.error());
-}
+
+  // Create a new employee
+  function addEmployee(employee) {
+    prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "What is employee's first name?"
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "What is employee's last name?"
+      },
+      {
+        type: "input",
+        name: "role",
+        message: "Enter the role id for this employee."
+      },
+      {
+        type: "input",
+        name: "manager_id",
+        message: "Please enter the manager id for this employee."
+      },
+    ]).then(res =>{
+      connection.query("INSERT INTO employee SET ?", {
+        first_name: res.first_name,
+        last_name: res.last_name,
+        role_id: res.role,
+        manager_id: res.manager_id
+      }, (err) => {
+        if (err) throw err;
+        console.log("successfully added employee");
+        init();
+    })
+    } );
+  }
+
+  // Remove an employee with the given id
+  function removeEmployee(employeeId) {
+    return this.connection.promise().query(
+      "DELETE FROM employee WHERE id = ?",(err,result) => {
+        console.table(result);
+        init();
+      });
+  }
+ 
 
 init();
 
